c09db945b20bec5af8877c316a916dda15e8770a
Rajesh Pitty <rajesh@rajeshpg.com>
2018-02-16 14:53:41 +0530

    chapter 6
    days of week and typecheck sample and exercises

--- src/chapter6/dayofweek.hs
+++ src/chapter6/dayofweek.hs
@@ -1,18 +1,20 @@
 module DayOfWeek where
 
-data DayOfWeek = Mon | Tue | Weds | Thu | Fri | Sat | Sun
+data DayOfWeek =
+  Mon | Tue | Weds | Thu | Fri | Sat | Sun
+  deriving (Eq, Show)
 
 data Date = Date DayOfWeek Int
 
-instance Eq DayOfWeek where
-  (==) Mon Mon = True
-  (==) Tue Tue = True
-  (==) Weds Weds = True
-  (==) Thu Thu = True
-  (==) Fri Fri = True
-  (==) Sat Sat = True
-  (==) Sun Sun = True
-  (==) _ _     = False
+-- instance Eq DayOfWeek where
+--   (==) Mon Mon = True
+--   (==) Tue Tue = True
+--   (==) Weds Weds = True
+--   (==) Thu Thu = True
+--   (==) Fri Fri = True
+--   (==) Sat Sat = True
+--   (==) Sun Sun = True
+--   (==) _ _     = False
 
 instance Eq Date where
   (==) (Date weekday dayOfMonth)
---
+++ src/chapter6/typecheck.hs
@@ -0,0 +1,50 @@
+module TypeCheck where
+
+data Person = Person Bool
+-- printPerson :: Person -> IO()
+-- printPerson person = putStrLn (show person) 
+
+
+data Mood = Blah | Woot deriving (Eq, Show, Ord)
+
+settleDown x = if x == Woot
+  then Blah
+else x
+
+type Subject = String
+type Verb = String
+type Object = String
+
+data Sentence =
+  Sentence Subject Verb Object
+  deriving (Eq, Show)
+
+s1 = Sentence "dogs" "drool"
+s2 = Sentence "Julie" "loves" "dogs"
+
+--exercises
+data Rocks =
+  Rocks String deriving (Eq, Show)
+
+data Yeah =
+  Yeah Bool deriving (Eq, Show)
+
+data Papu =
+  Papu Rocks Yeah
+  deriving (Eq, Show)
+
+-- doesnt type check 
+--phew = Papu "chases" True
+
+truth = Papu (Rocks "chomkydoz")
+        (Yeah True)
+
+equalityForall :: Papu -> Papu -> Bool
+equalityForall p p' = p == p'
+
+-- comparePapus :: Papu -> Papu ->Bool
+-- comparePapus p p' = p > p'
+        
+
+
+
