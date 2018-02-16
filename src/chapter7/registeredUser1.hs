7eea4cc2255bf876dedf1af2a72b327911b944d1
Rajesh Pitty <rajesh@rajeshpg.com>
2018-02-16 14:54:36 +0530

    chapter 7
    sample code: RegisteredUser

---
+++ src/chapter7/registeredUser1.hs
@@ -0,0 +1,21 @@
+module RegisteredUser where
+
+newtype Username =
+  Username String
+
+newtype AccountNumber =
+  AccountNumber Integer
+
+data User =
+  UnregisteredUser | RegisteredUser Username AccountNumber
+
+
+printUser :: User -> IO ()
+printUser UnregisteredUser =
+  putStrLn "Unregistereduser"
+printUser (RegisteredUser (Username name) (AccountNumber acctNum)) =
+  putStrLn $ name ++ " " ++ show acctNum
+
+  
+  
+ 
